b2908ae37b5a7da055713fdab051d97f7461b991
AkshayDhankhar1 <167584775+AkshayDhankhar1@users.noreply.github.com>
2025-08-09 00:14:11 +0530

    Time: 45 ms (16.8%), Space: 45.3 MB (14%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+void find(vector<vector<int>> &ans,int target,vector<int>& candidates,int inx,vector<int> a){
+    if(inx>=candidates.size()){
+        if(target==0){
+            ans.push_back(a);
+        }
+        return;
+    }
+    if(target>=candidates[inx]){
+    a.push_back(candidates[inx]);
+    find(ans,target-candidates[inx],candidates,inx,a);
+    a.pop_back();
+    }
+    find(ans,target,candidates,inx+1,a);
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int inx=0;
+        vector<int> a;
+        vector<vector<int>> ans;
+        find(ans,target,candidates,inx,a);
+        return ans;
+    }
+};
