ac34ba4ee02ccd0d8b8e9d13d2759a387087fb95
AkshayDhankhar1 <167584775+AkshayDhankhar1@users.noreply.github.com>
2025-10-01 20:50:52 +0530

    Time: 53 ms (45.6%), Space: 20.9 MB (52.79%) - LeetHub

---
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int find(int i,int j1,int j2,int n,int m,vector<vector<int>>&grid,vector<vector<vector<int>>>&dp){
+    if(j1<0 || j1>m-1 || j2<0 || j2>m-1) return -1e9;
+    if(i==n-1){
+        if(j1==j2) return grid[i][j1];
+        else return grid[i][j1]+grid[i][j2];
+    }
+    if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+    int maxi=INT_MIN;
+    for(int a=-1;a<2;a++){
+        for(int b=-1; b<=1; b++){
+                int value = 0;
+                if(j1==j2) value = grid[i][j1];
+                else value = grid[i][j1] + grid[i][j2];
+
+                value += find(i+1, j1+a, j2+b, n, m, grid, dp);
+                maxi = max(maxi, value);
+            }
+    }
+    return dp[i][j1][j2]=maxi;
+}
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n= grid.size();
+        int m= grid[0].size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        return find(0,0,m-1,n,m,grid,dp);
+    }
+};
