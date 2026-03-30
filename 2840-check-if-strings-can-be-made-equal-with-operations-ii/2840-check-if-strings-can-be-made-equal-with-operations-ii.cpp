0b7a3c3070af328c5453d105884fb3d5b4193ca6
AkshayDhankhar1 <167584775+AkshayDhankhar1@users.noreply.github.com>
2026-03-30 16:38:17 +0530

    Time: 6 ms (71.26%), Space: 19.8 MB (68.97%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -7,24 +7,19 @@ public:
         if(n1==1 && n2==1){
             return s1==s2;
         }
-        string odd1="";
-        string odd2="";
+        vector<int> odd1(26,0),even1(26,0),odd2(26,0),even2(26,0);
         for(int i=0;i<n1;i++){
-            if(i%2==1){
-                odd1+=s1[i];
+            if(i%2==0){
+                even1[s1[i]-'a']++;
             }
+            else odd1[s1[i]-'a']++;
         }
         for(int i=0;i<n2;i++){
-            if(i%2==1){
-                odd2+=s2[i];
+            if(i%2==0){
+                even2[s2[i]-'a']++;
             }
+            else odd2[s2[i]-'a']++;
         }
-        sort(odd1.begin(),odd1.end());
-        sort(odd2.begin(),odd2.end());
-        sort(s1.begin(),s1.end());
-        sort(s2.begin(),s2.end());
-        if(s1!=s2) return false;
-        if(odd1==odd2) return true;
-        return false;
+        return (odd1==odd2 && even1==even2);
     }
 };
